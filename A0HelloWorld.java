1a0ac204540bf21e1e83cc95fdca6486e4a99855
Antonio Luiz Neto <104032875+Oluizn@users.noreply.github.com>
2024-10-30 18:49:38 +0000

    atualizando

--- A0HelloWorld.java
+++ A0HelloWorld.java
@@ -1,7 +1,6 @@
 public class A0HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Alo Mundo\n");
+        System.out.println("Hello World\n");
         System.out.println("Alo Mundo");
     }
  }
- 
